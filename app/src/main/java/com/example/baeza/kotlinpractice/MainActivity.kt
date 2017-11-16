389f785f85a3c33cdc5c1c232ba6c65fc2bce38b
baeza <cbaezenko@gmail.com>
2017-11-16 18:33:58 +0300

    Initial commit

---
+++ app/src/main/java/com/example/baeza/kotlinpractice/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.baeza.kotlinpractice
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    val array = arrayOf("gato","perro","elefante")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        for (i in array.indices){ texto.append(array[i]+"\n") }
+        texto.setTextColor(resources.getColor(R.color.colorPrimaryDark))
+
+        button.setOnClickListener({
+            changeColor(resources.getColor(R.color.error_color_material))
+        })
+
+        button2.setOnClickListener({
+            changeColor(resources.getColor(R.color.primary_dark_material_light))
+        })
+    }
+
+    fun changeColor(color: Int){ texto.setTextColor(color)
+        array[2].capitalize()
+    }
+}
